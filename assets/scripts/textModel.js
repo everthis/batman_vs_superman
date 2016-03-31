1bf6099fc2d72e78880d4ed8cf2727b4963cd1e2
hejie03 <hejie03@baidu.com>
2016-03-31 16:41:42 +0800

    text pop.

--- assets/scripts/textModel.js
+++ assets/scripts/textModel.js
@@ -1,9 +1,4 @@
 export function textModel() {
-	let str = `
-<div class="text-model">
-		<p class="text"><div>Hi, 周六的安排是：</div><div>地点：UME华星，</div><div>午饭：重八牛府，</div><div>电影开始时间是下午2:30</div>
-		</p>
-</div>
-	`;
+	let str = '<div class="text-model"><p class="text"><div>Hi, 周六的安排是：</div><div>地点：UME华星，</div><div>午饭：重八牛府，</div><div>电影开始时间是下午2:30</div></p></div>';
 	return str;
 }
